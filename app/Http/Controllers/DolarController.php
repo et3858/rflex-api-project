80fa0b90833647f6489d295a9ca34d0f829eef67
Eduardo Tejada <et3858@gmail.com>
2024-02-08 12:57:41 -0300

    Implement validators

--- app/Http/Controllers/DolarController.php
+++ app/Http/Controllers/DolarController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Dolar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DolarController extends Controller
 {
-    public function get()
+    public function get(Request $req)
     {
+        $validator = Validator::make($req->all(), [
+            'start_date' => ['required', 'date'],
+            'end_date' => ['required', 'date', 'after_or_equal:start_date'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
         $dollars = Dolar::get();
 
-        return response($dollars);
+        return response()->json($dollars);
     }
 }
