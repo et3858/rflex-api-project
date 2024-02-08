b44c809524603cbece1a09ff615a23f6d066489c
Eduardo Tejada <et3858@gmail.com>
2024-02-08 10:58:45 -0300

    Define task schedule for updating dolars table

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,22 +2,17 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RegisterValues;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(RegisterValues::class)->hourly();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
