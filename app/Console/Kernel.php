ba369c8f70f4134c40bcf123a24a16d89d19611b
Eduardo Tejada <et3858@gmail.com>
2024-02-10 21:55:22 -0300

    Set current year as a parameter to run RegisterValues command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command(RegisterValues::class)->hourly();
+        $schedule->command(RegisterValues::class, [now()->year])->hourly();
     }
 
     protected function commands(): void
